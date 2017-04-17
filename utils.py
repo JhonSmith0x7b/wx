a26a5e250a4142721ac1895a70b71fb282389d98
JhonSmith0x7b <gugoyuoo@gmail.com>
2017-04-17 17:20:22 +0800

    add text reply

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import sys;reload(sys);sys.setdefaultencoding('utf-8')
 import common
 
-def print_log(str):
+def print_log(sth):
 	with open(common.LOG_PATH, 'a') as f:
-		f.write(str)
+		map(f.write, sth)
+		f.write('\n')
 		f.close()
--- wx_manager.py
+++ wx_manager.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from flask import request, g, render_template
 import common, hashlib, utils
+import xml.etree.ElementTree as et
 def request_bridge():
 	if request.method == 'GET':
 		manage = wx_manage()
@@ -12,7 +13,15 @@ def request_bridge():
 		return response
 		pass
 	elif request.method == 'POST':
-		return 'fatal.'
+                manage = wx_manage()
+                signature = request.args.get('signature', '')
+                timestamp = request.args.get('timestamp', '')
+                nonce = request.args.get('nonce', '')
+                echostr = request.args.get('echostr', '')
+                check_result = manage.check(signature, timestamp, nonce, echostr)
+		utils.print_log('check succ')
+		response = manage.deal_text_msg(request.data)
+		return response
 		pass
 	pass
 
@@ -22,17 +31,33 @@ class wx_manage:
 		pass
 
 	def check(self, signature, timestamp, nonce, echostr):
-		if signature == '' or timestamp == '' or nonce == '' or echostr == '':
-			return 'various is empty'
+		if signature == '' or timestamp == '' or nonce == '':
+			utils.print_log('some empty')
+			return ''
 		token = common.TOKEN
 		various = [token, nonce, timestamp]
 		various.sort()
 		sha1 = hashlib.sha1()
 		map(sha1.update, various)
 		hashcode = sha1.hexdigest()
+		utils.print_log(hashcode)
 		if hashcode == signature:
 			return echostr
 		else:
 			print hashcode
-			return 'check fatal.'
+			return '1'
+		pass
+	def deal_text_msg(self, data):
+		root = et.fromstring(data)
+		response_content = """
+		<xml>
+        <ToUserName><![CDATA[%s]]></ToUserName>
+        <FromUserName><![CDATA[%s]]></FromUserName>
+        <CreateTime><![CDATA[%s]]></CreateTime>
+        <MsgType><![CDATA[%s]]></MsgType>
+        <Content><![CDATA[%s]]></Content>
+        </xml>
+		""" % (root.findall('FromUserName')[0].text, root.findall('ToUserName')[0].text, root.findall('CreateTime')[0].text, 'text', 'hello world')
+		utils.print_log(response_content)
+		return response_content
 		pass
